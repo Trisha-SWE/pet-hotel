5ef64caa46e63ae53a8e8a1636d676b5da170bb4
Trisha-SWE <trisha2305341292@diu.edu.bd>
2025-03-28 20:54:44 +0600

    pet hotel multiple apge added and footer area updated

---
+++ src/main/java/org/springframework/samples/petclinic/system/FooterController.java
@@ -0,0 +1,18 @@
+package org.springframework.samples.petclinic.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FooterController {
+
+    @GetMapping("/footer")
+    public String footer(Model model) {
+        model.addAttribute("email", "petcare@gmail.com");
+        model.addAttribute("phone", "+8801900-000000");
+        model.addAttribute("developer", "Rukaiya Akter Trisha");
+        model.addAttribute("developerGithub", "https://github.com/Trisha-SWE");
+        return "footer";
+    }
+}
---
+++ src/main/java/org/springframework/samples/petclinic/system/PageController.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.system;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "privacy";
+    }
+    @GetMapping("/terms")
+    public String terms() {
+        return "terms";
+    }
+}
