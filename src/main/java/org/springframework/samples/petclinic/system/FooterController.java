237aac7d580e95d6fd0b11779e73329117b1ff8f
Trisha-SWE <trisha2305341292@diu.edu.bd>
2025-03-29 14:43:30 +0600

    footer updated

--- src/main/java/org/springframework/samples/petclinic/system/FooterController.java
+++ src/main/java/org/springframework/samples/petclinic/system/FooterController.java
@@ -9,7 +9,7 @@ public class FooterController {
 
     @GetMapping("/footer")
     public String footer(Model model) {
-        model.addAttribute("email", "petcare@gmail.com");
+        model.addAttribute("email", "Trisha292@diu.edu.bd");
         model.addAttribute("phone", "+8801900-000000");
         model.addAttribute("developer", "Rukaiya Akter Trisha");
         model.addAttribute("developerGithub", "https://github.com/Trisha-SWE");
